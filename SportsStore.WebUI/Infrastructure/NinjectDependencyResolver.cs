dcbf53394645d3ba0196ef0df8aac4a0fb1da7ad
wiizard chen <wiizard-chen@foxmail.com>
2016-11-06 20:38:09 +0800

    sada

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Ninject;
 
 namespace SportsStore.WebUI.Infrastructure
 {
-    public class NinjectDependencyResolver
+    public class NinjectDependencyResolver :IDependencyResolver
     {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+        private void AddBindings()
+        {
+
+        }
     }
 }
