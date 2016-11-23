e5b5b5e3463509c7ddcc5c9a075a4623fce771f0
wiizard chen <wiizard-chen@foxmail.com>
2016-11-23 11:53:53 +0800

    menu two simple views

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -16,11 +16,12 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category=null)
+        public PartialViewResult Menu(string category=null,bool horizontalLayout =false)
         {
             ViewBag.SelectedCategory = category;
             IEnumerable<string> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "Menu" : "MenuHorizontal";
+            return PartialView(viewName,categories);
         }
     }
 }
